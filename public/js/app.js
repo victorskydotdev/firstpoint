be27bc53954ee6476d28c19348a8c2675ba990f3
techyVS <victorsky90@gmail.com>
2023-07-21 08:46:28 +0100

    made some UI modification

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,26 @@ const burgerBtn = document.querySelector('.hamburger-menu');
 const nav = document.querySelector('.navbar');
 const navCloseBtn = document.querySelector('.hamburger-close');
 const readMore = document.querySelectorAll('.readmore-btn');
+const teamBox = document.querySelectorAll('.team-member-wrap');
+const expandedRead1 = document.querySelector('.expanded-read1');
+const expandedRead2 = document.querySelector('.expanded-read2');
+const expandedRead3 = document.querySelector('.expanded-read3');
 
+const closeBtn = document.querySelectorAll('.collapse');
+
+for (let i = 0; i < closeBtn.length; i += 1) {
+	closeBtn[i].addEventListener('click', () => {
+		console.log('closeButton clicked');
+
+		expandedRead1.classList.remove('readmore-active');
+		expandedRead2.classList.remove('readmore-active');
+		expandedRead3.classList.remove('readmore-active');
+
+		for (let j = 0; j < readMore.length; j++) {
+			readMore[j].style.display = 'block';
+		}
+	});
+}
 burgerBtn.addEventListener('click', () => {
 	nav.style.display = 'flex';
 	burgerBtn.classList.add('hidden');
@@ -16,37 +35,45 @@ navCloseBtn.addEventListener('click', () => {
 });
 
 const expandProfile = () => {
-	const readMoreOne = document.querySelector('.readmore-btn1');
+	// const readMoreOne = document.querySelector('.readmore-btn1');
 	for (let i = 0; i < readMore.length; i++) {
 		readMore[0].addEventListener('click', () => {
-			const expandedRead1 = document.querySelector('.expanded-read1');
-
 			expandedRead1.classList.add('readmore-active');
 			readMore[0].style.display = 'none';
 
 			const active = document.querySelector('.readmore-active');
 
 			if (active) {
-				const profileBox = document.querySelectorAll('.profile');
-
-				for (let i = 0; i < profileBox.length; i += 1) {
-					profileBox[0].classList.add('height');
+				for (let i = 0; i < teamBox.length; i += 1) {
+					teamBox[i].classList.add('height');
 				}
 			}
 		});
 
 		readMore[1].addEventListener('click', () => {
-			const expandedRead2 = document.querySelector('.expanded-read2');
-
 			expandedRead2.classList.add('readmore-active');
 			readMore[1].style.display = 'none';
+
+			const active = document.querySelector('.readmore-active');
+
+			if (active) {
+				for (let i = 0; i < teamBox.length; i += 1) {
+					teamBox[i].classList.add('height');
+				}
+			}
 		});
 
 		readMore[2].addEventListener('click', () => {
-			const expandedRead3 = document.querySelector('.expanded-read3');
-
 			expandedRead3.classList.add('readmore-active');
 			readMore[2].style.display = 'none';
+
+			const active = document.querySelector('.readmore-active');
+
+			if (active) {
+				for (let i = 0; i < teamBox.length; i += 1) {
+					teamBox[i].classList.add('height');
+				}
+			}
 		});
 	}
 };
